4808bdbe4e0bcbc8a9ebc3dcf463a99068ae4b4c
Fabbio <fabioamaral.93jr@gmail.com>
2021-11-09 18:57:41 -0300

    adicionando componentes produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { CadastrarProdutoComponent } from './produto/cadastrar-produto/cadastrar-produto.component';
+import { EditarProdutoComponent } from './produto/editar-produto/editar-produto.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    ProdutoComponent,
+    CadastrarProdutoComponent,
+    EditarProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'',component:ProdutoComponent},
+      {path:'produtos', component:ProdutoComponent},
+      {path:'produtos/cadastrar-produto', component:CadastrarProdutoComponent},
+      {path: 'produtos/editar-produto/:id', component: EditarProdutoComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
