e39fb3ffda97010460d3f1f02658ec3142047eae
Fabbio <fabioamaral.93jr@gmail.com>
2022-01-31 23:01:46 -0300

    ft(pedidos): adds pedidos folder and its files

---
+++ src/app/pedidos/pedidos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos',
+  templateUrl: './pedidos.component.html',
+  styleUrls: ['./pedidos.component.css']
+})
+export class PedidosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  buscarCliente() {
+
+  }
+
+}
